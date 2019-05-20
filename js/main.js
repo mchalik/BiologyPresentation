e5704ac12b5ea245d216dd43ae6f1f62339823a1
mchalik <maksicmhalik@gmail.com>
2019-05-20 03:23:22 +0300

    do refactoring, adjust animation

--- js/main.js
+++ js/main.js
@@ -1,30 +1,34 @@
+"use strict"
+
 document.getElementsByClassName('main__wrapper')[0].dataset.page = localStorage.getItem('page') || 0;
 document.getElementsByClassName('section--slided')[0].dataset.slide = localStorage.getItem('slide') || 0;
 
-let parallaxEvent = new CustomEvent('parallax');
+const parallaxEvent = new CustomEvent('parallax');
 
 
 (function scrollPages() {
+    const slidesContainer = document.querySelector('.main__wrapper');
     let initialPosition = null;
     let moving = false;
     let scrolled = false;
-    let slidesContainer = document.querySelector('.main__wrapper');
     let page = parseInt(slidesContainer.dataset.page);
+
     function scrollPageUp() {
-        if (page < 2) slidesContainer.dataset.page = ++page;
+        if (page >= 2) return;
+
+        slidesContainer.dataset.page = ++page;
         window.dispatchEvent(parallaxEvent);
         localStorage.setItem("page", page.toString());
     }
     function scrollPageDown() {
-        if (page > 0) {
-            slidesContainer.dataset.page = --page;
-            window.dispatchEvent(parallaxEvent);
-            localStorage.setItem("page", page.toString());
-        }
+        if (page <= 0) return;
 
+        slidesContainer.dataset.page = --page;
+        window.dispatchEvent(parallaxEvent);
+        localStorage.setItem("page", page.toString());
     }
 
-    let scrollStart = (e) => {
+    const scrollStart = (e) => {
         if (e.target.classList.contains('slider__ice'))  return;
 
         initialPosition = e.pageY;
@@ -32,12 +36,12 @@ let parallaxEvent = new CustomEvent('parallax');
     };
 
 
-    let scrollMove = (e) => {
+    const scrollMove = (e) => {
         if (!moving) return;
         if (scrolled) return;
-        let currentPosition = e.pageY;
-        let diff = initialPosition - currentPosition;
 
+        const currentPosition = e.pageY;
+        const diff = initialPosition - currentPosition;
 
         if (diff > 20) {
             scrollPageUp();
@@ -49,11 +53,10 @@ let parallaxEvent = new CustomEvent('parallax');
     };
 
 
-    let scrollEnd = (e) => {
+    const scrollEnd = (e) => {
         moving = false;
         scrolled = false;
     };
-
 
 
     if (window.PointerEvent) {
@@ -77,52 +80,59 @@ let parallaxEvent = new CustomEvent('parallax');
     const slider = document.querySelector('.slider__ice');
     const sliderFilledLine = document.querySelector('.slider__line--filled');
     const sliderLineWidth = document.querySelector('.slider__line').getBoundingClientRect().width;
+    const slideContainer = document.querySelector('.section--slided');
     let initialPosition = null;
     let moving = false;
     let sliderInitialPosition = 0;
-    const slideContainer = document.querySelector('.section--slided');
 
     (function adjustSlider() {
-        if (slideContainer.dataset.slide == 0) {
-            slider.style.left = 0 + '%';
-            sliderFilledLine.style.width = 0 + '%';
-        } else if(slideContainer.dataset.slide == 1) {
-            slider.style.left = 50 + '%';
-            sliderFilledLine.style.width = 50 + '%';
-        } else if(slideContainer.dataset.slide == 2) {
-            slider.style.left = 100 + '%';
-            sliderFilledLine.style.width = 100 + '%';
+        const curentSlide = parseInt(slideContainer.dataset.slide);
+
+        switch (curentSlide) {
+            case 1:
+                slider.style.left = 0 + '%';
+                sliderFilledLine.style.width = 0 + '%';
+                break;
+            case 2:
+                slider.style.left = 50 + '%';
+                sliderFilledLine.style.width = 50 + '%';
+                break;
+            case 3:
+                slider.style.left = 100 + '%';
+                sliderFilledLine.style.width = 100 + '%';
+                break;
         }
     }());
 
     function findLeftPosition(el) {
         const leftPositionString = window.getComputedStyle(el).left;
-        return parseFloat(leftPositionString.match(/.*(?=px)/)) / sliderLineWidth * 100;
+        const leftPositionNumber = parseFloat(leftPositionString.match(/\d+(\.\d+)?(?=px)/));
+        const leftPositionInPercents = leftPositionNumber / sliderLineWidth * 100;
+        return leftPositionInPercents;
     }
 
     function switchSlide() {
 
-        sliderEndPosition = findLeftPosition(slider);
+        let sliderEndPosition = findLeftPosition(slider);
 
         if (sliderEndPosition < 25) {
-            slideContainer.dataset.slide = 0;
+            slideContainer.dataset.slide = "0";
             localStorage.setItem("slide", "0");
         }
         else if (sliderEndPosition < 75) {
-            slideContainer.dataset.slide = 1;
+            slideContainer.dataset.slide = "1";
             localStorage.setItem("slide", "1");
         }
-        else if (sliderEndPosition < 100) {
-            slideContainer.dataset.slide = 2;
+        else if (sliderEndPosition <= 100) {
+            slideContainer.dataset.slide = "2";
             localStorage.setItem("slide", "2");
         }
-
     }
 
     const gestureStart = (e) => {
         initialPosition = e.pageX;
         moving = true;
-        sliderInitialPosition = findLeftPosition(slider) || 0;
+        sliderInitialPosition = findLeftPosition(slider);
 
     };
 
@@ -143,24 +153,27 @@ let parallaxEvent = new CustomEvent('parallax');
     };
 
     const gestureEnd = (e) => {
+        if (!moving) return;
+
         moving = false;
-        let sliderInitPosition = parseInt(findLeftPosition(slider));
+
+        let sliderReleasePosition = parseInt(findLeftPosition(slider));
         let sliderEndPosition = 0;
 
-        if (sliderInitPosition < 25) sliderEndPosition = 0;
-        else if (sliderInitPosition < 75) sliderEndPosition = 50;
-        else if (sliderInitPosition <= 100) sliderEndPosition = 100;
+        if (sliderReleasePosition < 25) sliderEndPosition = 0;
+        else if (sliderReleasePosition < 75) sliderEndPosition = 50;
+        else if (sliderReleasePosition <= 100) sliderEndPosition = 100;
 
         (function loop() {
+            if (sliderReleasePosition === sliderEndPosition) return;
 
-            if (sliderInitPosition === sliderEndPosition ) return;
-            if (sliderInitPosition > sliderEndPosition) {
-                sliderInitPosition--;
+            if (sliderReleasePosition > sliderEndPosition) {
+                sliderReleasePosition--;
             } else {
-                sliderInitPosition++;
+                sliderReleasePosition++;
             }
-            slider.style.left = sliderInitPosition + '%';
-            sliderFilledLine.style.width = sliderInitPosition + '%';
+            slider.style.left = sliderReleasePosition + '%';
+            sliderFilledLine.style.width = sliderReleasePosition + '%';
             setTimeout(loop, 8);
         }());
     };
@@ -186,28 +199,29 @@ let parallaxEvent = new CustomEvent('parallax');
 
 
 (function parallax() {
-    let iceElem = document.querySelectorAll('.js-parallax');
+    const screenHeight = 768;
+    const iceElem = document.querySelectorAll('.js-parallax');
+    const mainWrapper = document.querySelector('.main__wrapper');
+
     window.addEventListener("parallax", () => {
-        iceElem.forEach(function (e) {
-
-            const  screenHeight = 768;
-
+        iceElem.forEach((e) => {
             let paralaxed = false;
-            const mainWrapper = document.querySelector('.main__wrapper');
-            mainWrapper.addEventListener('transitionend', (e) => {
-                setTimeout((e) => {
+
+            mainWrapper.addEventListener('transitionend', () => {
+                setTimeout(() => {
                     paralaxed = true;
                 }, 1000);
             });
-            (function loop() {
 
-                let mainWrapperYPosition = mainWrapper.getBoundingClientRect().y;
-                let neededTranslate = (mainWrapperYPosition + screenHeight) / screenHeight  * 300;
-                e.style.transform = `translateY(${neededTranslate}px)`;
+            (function loop() {
                 if (paralaxed) return;
+
+                const mainWrapperYPosition = mainWrapper.getBoundingClientRect().y;
+                const neededTranslate = (mainWrapperYPosition + screenHeight) / screenHeight * 500;
+
+                e.style.transform = `translateY(${neededTranslate}px)`;
                 setTimeout(loop, 16);
-            }())
+            }());
         });
     });
-
 }());
